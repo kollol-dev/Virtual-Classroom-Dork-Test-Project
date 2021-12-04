3ccf9556c01b96f82093681447d8ed4cb9cb15ba
Kollol <kollol.work@gmail.com>
2021-12-04 22:18:22 +0600

    custom exception code updated

--- app/Exceptions/CustomException.js
+++ app/Exceptions/CustomException.js
@@ -4,24 +4,23 @@ const { LogicalException } = require("@adonisjs/generic-exceptions");
 
 class CustomException extends LogicalException {
   handle(error, { response }) {
+    let message = "";
     switch (error.status) {
+      
       case 400:
-        return response.status(400).json({
-          message: "Bad Request",
-        });
+        message = "Bad Request";
       case 401:
-        return response.status(401).json({
-          message: "You are not authorized for further operation",
-        });
+        message = "You are not authorized for further operation";
       case 403:
-        return response.status(403).json({
-          message: "Forbidden",
-        });
+        message = "Forbidden";
       case 405:
-        return response.status(405).json({
-          message: "Not Allowed",
-        });
+        message = "Not Allowed";
     }
+
+    return response.status(400).json({
+      message: message,
+      status: "Fail",
+    });
   }
 }
 
