72e65291867f0a0d882798d38e20ebf16ce341d8
Kollol <kollol.work@gmail.com>
2021-12-05 18:33:46 +0600

    pagination of remaining poxt(exam/assignment) added

--- app/Controllers/Http/Student/ClassRoomPostController.js
+++ app/Controllers/Http/Student/ClassRoomPostController.js
@@ -1,6 +1,37 @@
-'use strict'
+"use strict";
+
+// Models
+const Student = use("App/Models/Student");
+const Classroom = use("App/Models/Classroom");
+const ClassroomPost = use("App/Models/ClassroomPost");
+const ClassroomSubscribedStudent = use("App/Models/ClassroomSubscribedStudent");
+
+// Plugins & Exceptions
+const CustomException = use("App/Exceptions/CustomException");
+const { validate } = use("Validator");
+
 
 class ClassRoomPostController {
+
+    async paginateClassRoomPostsByStudnetId({ request, response, auth }) {
+        let page = request.input('page', 1)
+
+        try {
+            var student = await auth.authenticator('student').getUser()
+        } catch (error) {
+            throw new CustomException("", 401, "")
+        }
+
+        let classRoomIds = await ClassroomSubscribedStudent.where('student_id', student.id)
+            .pluck('classroom_id');
+
+        return response.status(200).json({
+            status: 'Success',
+            response: await ClassroomPost.query()
+                .whereIn('id', classRoomIds)
+                .paginate(page, pagination)
+        })
+    }
 }
 
 module.exports = ClassRoomPostController
