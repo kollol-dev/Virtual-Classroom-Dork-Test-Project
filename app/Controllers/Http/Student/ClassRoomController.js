123d053d0e2d10233ca099a7929aa26015acd280
Kollol <kollol.work@gmail.com>
2021-12-05 18:03:37 +0600

    student subscribe and account creation added

---
+++ app/Controllers/Http/Student/ClassRoomController.js
@@ -0,0 +1,83 @@
+"use strict";
+
+// Models
+const Student = use("App/Models/Student");
+const Classroom = use("App/Models/Classroom");
+const ClassroomSubscribedStudent = use("App/Models/ClassroomSubscribedStudent");
+
+// Plugins & Exceptions
+const CustomException = use("App/Exceptions/CustomException");
+const { validate } = use("Validator");
+const { pagination } = require("../../../Modules/Common");
+
+class ClassRoomController {
+    async subscribeToClass({ request, response, auth }) {
+        const rules = {
+            fullName: "required|string|max:254",
+            email: "required|email|unique:students,email|max:100",
+            school_id: "required|integer|max:60",
+            invitation_code: 'required|string|max:254',
+        };
+
+        // check for classroom
+        let checkCounter = await Classroom.query()
+            .where("invitation_code", request.body.invitation_code)
+            .where("status", "running")
+            .getCount();
+
+        /* Classroom not found  */
+        if (!checkCounter) {
+            throw new CustomException(
+                "Classroom not found or this classroom has ended already.",
+                400,
+                ""
+            );
+        }
+
+        try {
+            var [student, classroom] = await Promise.all([
+                Student.findOrCreate(
+                    { email: request.body.email },
+                    {
+                        fullName: request.body.fullName,
+                        email: request.body.email,
+                        school_id: request.body.school_id,
+                    }
+                ),
+                Classroom.query()
+                    .where("invitation_code", request.body.invitation_code)
+                    .where("status", "running")
+                    .first()
+            ])
+        } catch (error) {
+            /* Something went worng  */
+            throw new CustomException("", 500, "");
+        }
+
+        let checkCounter = await ClassroomSubscribedStudent.query()
+            .where("classroom_id", classroom.id)
+            .where("student_id", student.id)
+            .getCount();
+
+        /* Already subscribed  */
+        if (checkCounter) {
+            throw new CustomException(
+                "You have already subscribed this classroom.",
+                403,
+                ""
+            );
+        }
+
+        await ClassroomSubscribedStudent.create({
+            classroom_id: classroom.id,
+            student_id: student.id,
+        });
+
+        return response.status(200).json({
+            status: "Success",
+            message: "Classroom subscibed.",
+        });
+    }
+}
+
+module.exports = ClassRoomController;
---
+++ app/Controllers/Http/Student/ClassRoomPostController.js
@@ -0,0 +1,6 @@
+'use strict'
+
+class ClassRoomPostController {
+}
+
+module.exports = ClassRoomPostController
