456fc98338d5268246d97b53e5cbfacaf4a040c2
Kollol <kollol.work@gmail.com>
2021-12-05 17:32:20 +0600

    route for student added

---
+++ start/routes/v1/student.js
@@ -0,0 +1,32 @@
+"use strict";
+const Route = use("Route");
+const CustomException = use("App/Exceptions/CustomException");
+
+/* ***********************   api prefix = '/api/v1/student/*'   *********************** */
+
+Route.group(() => {
+
+    Route.post("/classroom/subscribe", 'ClassRoomController.subscribeToClass')
+
+}).namespace("Student")
+    .prefix("/api/v1/student")
+    .middleware(['studentLoggedIn']);
+
+
+
+// other routes with middleware
+Route.group(() => {
+
+    /* Classroom post routes */
+    Route.get('/classroom/:class_id/posts/get/paginate', 'ClassRoomPostController.paginateClassRoomPostsByStudnetId');
+    Route.post('/classroom/:class_id/posts/result/submit', 'ClassRoomPostController.submitResultByClassId');
+    /* Classroom post routes ends */
+
+
+    Route.any('*', () => {
+        throw CustomException("", 404, "");;
+    })
+})
+    .namespace("Student")
+    .prefix("/api/v1/student")
+    .middleware(["student"]);
